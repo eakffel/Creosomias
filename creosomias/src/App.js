a1f0d88741c0fa826e5ce293b406893b6fb069ee
eakffel <60756427+eakffel@users.noreply.github.com>
2023-05-22 06:53:01 -0500

    a
    a

---
+++ creosomias/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { Layout } from "./Layout.js";
+
+
+export default function App() {
+  return (
+    <Layout>
+      <Navbar isBordered variant="floating">
+        <Navbar.Brand>
+          <Text b color="inherit" hideIn="xs">
+         Creosomias
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs" variant="highlight-rounded">
+          <Navbar.Link href="#">Features</Navbar.Link>
+          <Navbar.Link isActive href="#">Customers</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Company</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Link color="inherit" href="#">
+            Login
+          </Navbar.Link>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+    </Layout>
+  )
+}
+
