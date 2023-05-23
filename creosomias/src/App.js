fd1b5917f7e65b07ec797545c5091a4a2c544dd0
eakffel <60756427+eakffel@users.noreply.github.com>
2023-05-23 17:15:32 -0500

    s
    s

--- creosomias/src/App.js
+++ creosomias/src/App.js
@@ -1,35 +1,25 @@
-import './App.css';
-import { Navbar, Button, Link, Text } from "@nextui-org/react";
-import { Layout } from "./Layout.js";
+import React from 'react';
+import home from './home.js'
 
-export default function App() {
-  const 
+function Navbar() {
   return (
-    <Layout>
-      <Navbar isBordered variant="floating">
-        <Navbar.Brand>
-          <Text b color="inherit" hideIn="xs">
-         Creosomias
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content hideIn="xs" variant="highlight-rounded">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link isActive href="#">Customers</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
-    </Layout>
-  )
+    <nav>
+      <ul>
+        <li>
+          <a href="./home.js">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
+export default Navbar;
